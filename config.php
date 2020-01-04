26758a27a9df7c9d8c4e63f50b73100d35718c49
vultr97 <53869016+vultr97@users.noreply.github.com>
2020-01-04 16:25:18 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-define('APIKEY', 'AIzaSyB5zY2fq6YRSMxF7HwGb8rEQ8VVRxiO6ps');
+define('APIKEY', 'AIzaSyAasmJryLA2hOiwojP1advOkEIDc0Jm-Q8 ');
 define('GJ_CODE', 'HK');
 define('SITE_NAME', 'you2b1eclub');
 define('TITLENAME', 'you2beclub');
 define('EN2DEKEY', '32123wwerwertgfdgds');
 define('EMAIL', '1334347212@qq.com');
 define('NAME', 'admin');
-define('PASSWORD', 'admin');
+define('PASSWORD', 'cherry');
 define('LOGINHTML', '<!DOCTYPE html>
 <html lang="en">
 <head>
